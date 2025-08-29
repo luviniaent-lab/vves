0b6717dc4038806934206c9861346a3a741c1099
luviniaent-lab <luvinia.ent@gmail.com>
2025-08-30 01:25:24 +0530

    Fix Footer JSX parsing error

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,8 @@ import { Mail, Phone, MapPin, Facebook, Twitter, Youtube, Linkedin, ExternalLink
 
 export default function Footer() {
   return (
-    <footer className="bg-indian-maroon text-white">
+    <>
+      <footer className="bg-indian-maroon text-white">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {/* Organization Info */}
@@ -147,6 +148,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-    </footer>
+      </footer>
+    </>
   )
 }
